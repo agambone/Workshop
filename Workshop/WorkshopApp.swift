b8b7b7313054c284c16df3edb4a18901d61383cc
Antonio Gambone <agambone911@gmail.com>
2024-11-04 10:36:55 +0100

    Initial Commit

---
+++ Workshop/WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopApp.swift
+//  Workshop
+//
+//  Created by Antonio Gambone on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
